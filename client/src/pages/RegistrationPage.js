f9f15033f1a0a725120e006f7e156a8dc03140e5
MarcoGuzman-Balcazar <maguzm2@emory.edu>
2025-07-16 11:43:16 -0400

    Updating RegistrationPage.js

--- client/src/pages/RegistrationPage.js
+++ client/src/pages/RegistrationPage.js
@@ -4,11 +4,35 @@ import "./RegistrationPage.css"
 function RegistrationPage() {
 
   const [action, setAction] = useState("Sign Up");
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
+  const [formData, setFormData] = useState({ email: "", password: "" })
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  }
 
-  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const endpoint = 'http://localhost:5000/api/register'
+
+    console.log(formData)
+    try {
+      const response = await fetch(endpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData)
+      });
+
+      const result = await response.json()
+      alert(result.message)
+
+    } catch (error) {
+      console.error("Error", error)
+      alert("Failed to connect to server")
+    }
+  };
 
   return (
     <div className='container'>
@@ -17,15 +41,19 @@ function RegistrationPage() {
         <div className='underline'></div>
       </div>
       <div className="inputs">
-        <div className="input" >
-            <input type="email" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)}/>
-        </div>
-        <div className="input">
-            <input type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)}/>
-        </div>
+        <form onSubmit={handleSubmit} >
+          <div className="input">
+            <label htmlFor="email">Enter your email </label>
+            <input type= "email" name="email" value={FormData.email} onChange={handleChange} required />
+          </div>
+          <div className="input">
+            <label hrmlFor="password">Enter your password: </label>
+            <input type= "password" name="password" value={FormData.password} onChange={handleChange}required />
+          </div>
+        </form>
       </div>
       <div className='submit-container'>
-        <div className={action==="Login"?"submit gray":"submit"} onClick={() =>{setAction("Sign Up")}}>Sign Up</div>
+        <div className={action==="Login"?"submit gray":"submit"} onClick={handleSubmit}>Sign Up</div>
         <div className={action==="Submit"?"submit gray":"submit"} onClick={() =>{setAction("Login")}}>Log In</div>
       </div>
     </div>
