015b1e3aceca7f6440d5c06ce9b6abf9fcf74fd2
MarcoGuzman-Balcazar <maguzm2@emory.edu>
2025-07-16 11:37:57 -0400

    Created registration and sent the data to the backend

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 import sqlite3
 
@@ -8,11 +8,21 @@ def get_db_connection():
     return conn
 
 app = Flask(__name__)
-CORS(app) # Enable CORS for your React app to access Flask API
+CORS(app, origins=['http://localhost:3000']) # Enable CORS for your React app to access Flask API
 
 @app.route('/api/data')
 def get_data():
     return jsonify({"message": "Hello from Flask!"})
+
+@app.route('/api/register', methods=['POST'])
+def register():
+    data = request.json
+    if not data:
+        return jsonify({'message': 'Invalid JSON'}), 400
+    email = data.get("email")
+    password = data.get("password")
+    return jsonify({'message': "Registered successfully!"})
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
